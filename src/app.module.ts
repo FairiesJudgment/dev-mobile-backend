ea2894ec60431caaa4a351d76d16da331d8de4fc
mattheo <matdas4427@gmail.com>
2024-10-29 18:10:19 +0100

    feat : games folder init

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,13 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ManagerModule } from './manager/manager.module';
 import { SellerModule } from './seller/seller.module';
+import { GamesController } from './games/games.controller';
+import { GamesService } from './games/games.service';
+import { GamesModule } from './games/games.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule, ManagerModule, SellerModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule, ManagerModule, SellerModule, GamesModule],
+  controllers: [GamesController],
+  providers: [GamesService],
 })
 export class AppModule {}
