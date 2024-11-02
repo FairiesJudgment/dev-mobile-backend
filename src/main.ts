aa0ba7a5268fc88cee7fa54ccf4eaa04d56a64b9
mattheo <matdas4427@gmail.com>
2024-11-02 14:39:11 +0100

    feat: Added CORS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({transform : true}));
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  // Utiliser CORS
+  app.use(cors({
+    origin: configService.get('FRONTEND_URL'),
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.listen(configService.get<number>('PORT') ?? 3000);
 }
 bootstrap();
